add8b16e95f283b0acbca881492938c1bc0b7a83
Code Sandbox <hello@codesandbox.io>
2022-03-18 20:24:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8ffbc94ebeaf734ab61c34d2af228b15.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e9894547bf98eb425493e9f52a3751",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/index.html"
+  },
+  {
+    "revision": "ce17312b89fe527fdb24",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/css/main.dc14f345.chunk.css"
+  },
+  {
+    "revision": "1fb4641ce3ffa0cd62a2",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/js/2.de26a51a.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/js/2.de26a51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce17312b89fe527fdb24",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/js/main.fdaf4fee.chunk.js"
+  },
+  {
+    "revision": "de75398a052d192b3102",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/js/runtime-main.ce827c04.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "https://tejas-kotian.github.io/csb-2w6vxr/static/media/react.f370c1e0.png"
+  }
+]);
